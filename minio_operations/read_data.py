b55e1b922f45234174d5da9f3e6ed648e70cf275
Vũ Quang Huy <85333263+huyvu1404@users.noreply.github.com>
2024-11-19 17:57:39 +0700

    Delete minio_operations directory

--- minio_operations/read_data.py
+++
@@ -1,9 +0,0 @@
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-
-def read_data_from_minio(file_name):
-    s3_hook = S3Hook(aws_conn_id='minio_connect')
-    file = s3_hook.read_key(
-        key=f'raw/{file_name}.csv',
-        bucket_name='datalake'
-    )
-    return file
